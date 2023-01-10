15cdbb49f22da3b5d8e89850468d3fcae8efc1ad
Lindapazw <lindacsndelapaz@gmail.com>
2023-01-09 21:48:33 -0300

    add cuerpo de la historia

--- historiass_locas.py
+++ historiass_locas.py
@@ -5,3 +5,12 @@
 empresa = 'Coderhouse'
 
 print('Aprende a programar en: ' + empresa )
+
+# Mad Libs 
+
+adj = input("Adjetivo: ")
+verbo1 = input("Verbo: ")
+verbo2 = input("Verbo: ")
+sustantivo_plural = input("Sustantivo (plural): ")
+
+madlib = f"Programar es tan {adj} siempre me emociona porque me encanta {verbo1} problemas. ¡Aprende a {verbo2} con Coderhouse y alcanza tus {sustantivo_plural}!" 
